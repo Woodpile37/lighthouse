420b1b4d109d10b73024e5d6032ffc08b6719b9e
Diva M <divma@protonmail.com>
2022-11-28 14:13:12 -0500

    wip

--- beacon_node/network/src/router/processor.rs
+++ beacon_node/network/src/router/processor.rs
@@ -221,7 +221,7 @@ impl<T: BeaconChainTypes> Processor<T> {
         );
 
         if let RequestId::Sync(id) = request_id {
-            self.send_to_sync(SyncMessage::RpcBlob {
+            self.send_to_sync(SyncMessage::RpcGlob {
                 peer_id,
                 request_id: id,
                 blob_sidecar,
@@ -293,7 +293,7 @@ impl<T: BeaconChainTypes> Processor<T> {
             "Received BlockAndBlobssByRoot Response";
             "peer" => %peer_id,
         );
-        self.send_to_sync(SyncMessage::RpcBlockAndBlob {
+        self.send_to_sync(SyncMessage::RpcBlockAndGlob {
             peer_id,
             request_id,
             block_and_blobs,
