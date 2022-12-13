91ba0068c4106cc1ddc3259c44cfa478e6b16b0a
ethDreamer <37123614+ethDreamer@users.noreply.github.com>
2022-12-13 10:50:24 -0600

    Fixed Clippy Complaints & Some Failing Tests (#3791)
    * Fixed Clippy Complaints & Some Failing Tests
    * Update Dockerfile to Rust-1.65
    * EF test file renamed
    * Touch up comments based on feedback

--- beacon_node/execution_layer/src/lib.rs
+++ beacon_node/execution_layer/src/lib.rs
@@ -1466,7 +1466,7 @@ impl<T: EthSpec> ExecutionLayer<T> {
         let transactions = VariableList::new(
             block
                 .transactions()
-                .into_iter()
+                .iter()
                 .map(|transaction| VariableList::new(transaction.rlp().to_vec()))
                 .collect::<Result<_, _>>()
                 .map_err(ApiError::DeserializeTransaction)?,
