c5d0bbe98bf50488b3b792c204a8fc7518d02526
realbigsean <sean@sigmaprime.io>
2022-12-20 18:56:07 -0500

    fix rate limits, and a couple other bugs

--- beacon_node/network/src/beacon_processor/worker/rpc_methods.rs
+++ beacon_node/network/src/beacon_processor/worker/rpc_methods.rs
@@ -361,7 +361,7 @@ impl<T: BeaconChainTypes> Worker<T> {
         // Pick out the required blocks, ignoring skip-slots.
         let mut last_block_root = req
             .start_slot
-            .checked_sub(0)
+            .checked_sub(1)
             .map(|prev_slot| {
                 self.chain
                     .block_root_at_slot(Slot::new(prev_slot), WhenSlotSkipped::Prev)
@@ -540,7 +540,7 @@ impl<T: BeaconChainTypes> Worker<T> {
         // Pick out the required blocks, ignoring skip-slots.
         let mut last_block_root = req
             .start_slot
-            .checked_sub(0)
+            .checked_sub(1)
             .map(|prev_slot| {
                 self.chain
                     .block_root_at_slot(Slot::new(prev_slot), WhenSlotSkipped::Prev)
