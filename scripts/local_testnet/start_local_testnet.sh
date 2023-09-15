ee696bb45c0f0bee765258dfb28eb6cf173a0856
Age Manning <Age@AgeManning.com>
2023-09-15 03:07:24 +0000

    Add Experimental QUIC support (#4577)
    This PR adds QUIC support to Lighthouse. As this is not officially spec'd this will only work between lighthouse <-> lighthouse connections. We attempt a QUIC connection (if the node advertises it) and if it fails we fallback to TCP.
    
    This should be a backwards compatible modification. We want to test this functionality on live networks to observe any improvements in bandwidth/latency.
    
    NOTE: This also removes the websockets transport as I believe no one is really using it. It should be mentioned in our release however.
    
    Co-authored-by: João Oliveira <hello@jxs.pt>

--- scripts/local_testnet/start_local_testnet.sh
+++ scripts/local_testnet/start_local_testnet.sh
@@ -132,7 +132,7 @@ sed -i 's/"shardingForkTime".*$/"shardingForkTime": 0,/g' genesis.json
 for (( bn=1; bn<=$BN_COUNT; bn++ )); do
     secret=$DATADIR/geth_datadir$bn/geth/jwtsecret
     echo $secret
-    execute_command_add_PID beacon_node_$bn.log ./beacon_node.sh $SAS -d $DEBUG_LEVEL $DATADIR/node_$bn $((BN_udp_tcp_base + $bn)) $((BN_http_port_base + $bn)) http://localhost:$((EL_base_auth_http + $bn)) $secret
+    execute_command_add_PID beacon_node_$bn.log ./beacon_node.sh $SAS -d $DEBUG_LEVEL $DATADIR/node_$bn $((BN_udp_tcp_base + $bn)) $((BN_udp_tcp_base + $bn + 100)) $((BN_http_port_base + $bn)) http://localhost:$((EL_base_auth_http + $bn)) $secret
 done
 
 # Start requested number of validator clients
